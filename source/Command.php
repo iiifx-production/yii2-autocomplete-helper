49a4112285a7a64382521e444d0c996752fd6719
Vitaliy IIIFX Khomenko <iiifx@yandex.ru>
2015-09-11 18:03:21 +0300

    Наработки

--- source/Command.php
+++ source/Command.php
@@ -2,11 +2,38 @@
 
 namespace iiifx\yii2\autocomplete;
 
+use yii\helpers\FileHelper;
+
 class Command
 {
+    /**
+     *
+     */
     public static function execute ()
     {
-        echo __METHOD__;
+        ( new self() )->checkSimple()->checkAdvanced();
+    }
+
+
+    /**
+     * @return $this
+     */
+    protected function checkSimple ()
+    {
+        return $this;
+    }
+
+    protected function checkAdvanced ()
+    {
+        echo $this->getApplicationPath();
+    }
+
+    /**
+     * @return string
+     */
+    public function getApplicationPath ()
+    {
+        return FileHelper::normalizePath( dirname( dirname( dirname( __DIR__ ) ) ) );
     }
 }
 
